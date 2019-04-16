e526b0afa4522a555e181be1a75bc26b33b86ec9
Johnny Moreira <johnnymoreira91@hotmail.com>
2019-04-16 12:57:55 -0300

    Primeiro Commit Johnny

---
+++ app.js
@@ -0,0 +1,27 @@
+var app = require('./config/server');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/css/style.css', function(req, res){
+  res.sendFile(__dirname + '/css/style.css');
+});
+
+app.listen(3000, function(){
+    console.log('Servidor ON');
+});
+
+
+/*
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  //Open a file on the server and return its content:
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+
+  */
