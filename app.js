48f18ad90290c99afd131739bdce74a33b743969
Johnny Moreira <johnnymoreira91@hotmail.com>
2019-04-19 23:49:43 -0300

    Alterado visualizacao, estruturado projeto, agora paginas em ejs, caminho statico para css e imagens, usando consign

--- app.js
+++ app.js
@@ -1,93 +1,10 @@
 var app = require('./config/server');
+const endereco = 'localhost:';
+const port = 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/app/views/home/index.html');
-});
+//var rotalogin = require('./app/routes/login/login')(app);
 
-app.get('/menuprincipalsecretaria', function(req, res){
-  res.sendFile(__dirname + '/app/views/secretaria/menuprincipal/menuprincipal.html');
-});
-
-app.get('/construcaos', function(req, res){
-  res.sendFile(__dirname + '/app/views/secretaria/construcao/construcao.html');
-});
-
-app.get('/addagendasecretaria', function(req, res){
-  res.sendFile(__dirname + '/app/views/secretaria/agenda/addagenda.html');
-});
-
-app.get('/consultaagendasecretaria', function(req, res){
-  res.sendFile(__dirname + '/app/views/secretaria/agenda/consultaAgenda.html');
-});
-
-app.get('/addpacientesecretaria', function(req,res){
-  res.sendFile(__dirname + '../views/secretaria/paciente/cadastroPaciente.html');
-});
-
-app.get('/abouts', function(req, res){
-  res.sendFile(__dirname + '/app/views/secretaria/about/about.html');
-});
-
-
-// -------------------- ACIMA FINALIZA MENU SECRETARIA ------------------------ //
-
-app.get('/menuprincipalfisio', function(req, res){
-  res.sendFile(__dirname + '/app/views/fisio/menuprincipal/menuprincipalfisio.html');
-});
-
-app.get('/aboutf', function(req, res){
-  res.sendFile(__dirname + '/app/views/fisio/about/about.html');
-});
-
-app.get('/construcaof', function(req, res){
-  res.sendFile(__dirname + '/app/views/fisio/construcao/construcao.html');
-});
-
-app.get('/consultaagendafisio', function(req, res){
-  res.sendFile(__dirname + '/app/views/fisio/agenda/consultaAgenda.html');
-});
-
-// -------------------- ACIMA FINALIZA MENU FISIO ------------------------ //
-
-app.get('/css/style.css', function(req, res){
-  res.sendFile(__dirname + '/app/views/css/style.css');
-});
-
-app.get('/css/style1.css', function(req, res){
-  res.sendFile(__dirname + '/app/views/css/style1.css');
-});
-
-app.get('/css/menuprincipal.css', function(req, res){
-  res.sendFile(__dirname + '/app/views/css/menuprincipal1.css');
-});
-
-// ---------------- FIM IMAGENS -------------- //
-
-app.get('/imagens/doc.jpg', function(req, res){
-  res.sendFile(__dirname + '/app/views/imagens/doc.jpg');
-});
-
-app.get('/imagens/construcao.jpg', function(req, res){
-  res.sendFile(__dirname + '/app/views/imagens/construcao.jpg');
-});
-
-
-// ---------------- FIM IMAGENS -------------- //
-
-app.listen(3000, function(){
-    console.log('Servidor ON');
-});
-
-
-/*
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  //Open a file on the server and return its content:
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-
-  */
+app.listen(port, function(){
+    console.log('Servidor rodando com express, nodemom, consign, ejs');
+    console.log('Servidor Rodando em ', endereco,port);
+})
--- config/server.js
+++ config/server.js
@@ -1,5 +1,12 @@
 var express = require('express');
+var consign = require('consign');
 var app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+app.use('/assets/', express.static('assets'));
+//app.set('routes', './app/routes');
+
+consign().include('./app/routes').into(app);
 
 module.exports = app;
